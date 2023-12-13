835ab6eded9218a30aa01596f85b75d2d5c2e025
TDCole <61239276+TDCole21@users.noreply.github.com>
2023-12-13 09:41:31 +0000

    Update database_setup.sql

--- data/database_setup.sql
+++ data/database_setup.sql
@@ -1,21 +1,4 @@
--- Database setup
-
--- From Windows
--- I created an EC2 instance running Linux, and saved the PEM file in this directory
--- Make sure the EC2 instance is running, then in VSCode terminal, run:
--- ssh -i "quiz-rds.pem" ubuntu@ec2-34-245-51-155.eu-west-1.compute.amazonaws.com
--- Note, when you stop and start the instance, it'll have a different IP address, so this code will need to be altered.
--- Then on the EC2 Linux instance, run the below linux command
-
--- From Linux
--- mysql -h quiz.cbpf0qmeaxbr.eu-west-1.rds.amazonaws.com -P 3306 -u admin -p
--- PaB98CeB65OaK91
-
--- On Local Machine
--- I installed mysql 8 on the machine and start the service
--- Then using credentials in info.py I connect to the database 
-
-
+-- Creates Tables in MySQL for the app to work
 BEGIN;
 
 CREATE DATABASE IF NOT EXISTS quiz;
